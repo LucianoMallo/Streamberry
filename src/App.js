96c4b10bb97aa93d4a9756cc82d6a5cde26ebb09
Luciano Mallo <malloluciano@gmail.com>
2023-06-29 10:20:17 +0200

    add all the logic left

--- src/App.js
+++ src/App.js
@@ -2,23 +2,37 @@ import './App.css'
 import { useMovies } from './hooks/useMovies'
 import { Movies } from './components/Movies'
 import { useSearch } from './hooks/useSearch'
+import { useState, useCallback } from 'react'
+import debounce from 'just-debounce-it'
 
 function App () {
-  const { movies } = useMovies()
   const { search, updateSearch, error } = useSearch()
+  const [sort, setSort] = useState(false)
+  const { movies, getMovies, loading, fetchError } = useMovies({ search, sort })
+
+  const handleSort = () => {
+    setSort(!sort)
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log({ search })
+    getMovies({ search })
   }
   const handleChange = (event) => {
-    updateSearch(event.target.value)
+    const newSearch = event.target.value
+    updateSearch(newSearch)
+    debouncedHandleChange(newSearch)
   }
+  const debouncedHandleChange = useCallback(
+    debounce(search => {
+      getMovies({ search })
+    }, 300)
+    , [getMovies])
 
   return (
     <div className='page'>
       <header className='header'>
-        <h1 className='title'>Streamberry</h1>
+        <h1 className='title'>STREAMBERRY</h1>
         <form className='form' onSubmit={handleSubmit}>
           <input
             style={{
@@ -27,13 +41,23 @@ function App () {
             }} onChange={handleChange} value={search} name='search' type='text' placeholder='Avenger, Star Wars, the...'
           />
           <button type='submit'>Search</button>
-
         </form>
+
+        <div className='sort'>
+          <p className='sortText' htmlFor='sort'>Sort by Year</p>
+          <input className='sortCheckbox' type='checkbox' onChange={handleSort} checked={sort} />
+        </div>
+
         {error && <p style={{ color: 'red' }} className='error'>{error}</p>}
+        {search && <h2>Results for: {search}</h2>}
       </header>
 
       <main className='main'>
-        <Movies movies={movies} />
+        {loading
+          ? <p>Loading...</p>
+          : <Movies movies={movies} />}
+        {fetchError && <p style={{ color: 'red' }} className='error'>{fetchError}</p>}
+
       </main>
 
     </div>
